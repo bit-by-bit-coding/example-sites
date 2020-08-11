4455a8b3cea51c3247c424ca7757f7083721b23a
luciazhang00791 <67433805+luciazhang00791@users.noreply.github.com>
2020-08-11 00:09:34 -0700

    stay with class, sell wand, return wand paths finished

--- Basic Site/script.js
+++ Basic Site/script.js
@@ -21,8 +21,50 @@ function exploreHallway() {
     aButtonElement.onclick = die;
     bButtonElement.onclick = findClass;
 }
+function findClass() {
+    pElement.innerText = "Whew, that was a close one. You run back to your class as quickly as you can. You follow your leader around for a few hours until you spy a suspicious path of...sparkles? Weird.";
+    aButtonElement.innerText = "Follow the path!";
+    bButtonElement.innerText = "Stay with class!";
+    aButtonElement.onclick = followSparkles;
+    bButtonElement.innerText = stayWithClass;
+}
 function die() {
     pElement.innerText = "Looks like you never should have tried to find out what the mysterious sound was! Turns out, it was a teleportation spell used by one of the mysterious guardians of the museum that you got caught up in. When you did, it ripped apart your body into billions of atoms that were impossible to put back together! Oops!! Reload the page to try again!"
+    aButtonElement.innerText = "Go back to museum";
+    bButtonElement.innerText = "Go back to city";
+    aButtonElement.onclick = goToMuseum;
+    bButtonElement.onclick = goToCity;
+}
+function followClass() {
+    pElement.innerText = "Looks like you stay with the class! You follow your leader around for a few hours until you spy a suspicious path of...sparkles? Weird.";
+    aButtonElement.innerText = "Follow the path!";
+    bButtonElement.innerText = "Stay with class!";
+    aButtonElement.onclick = followSparkles;
+    bButtonElement.innerText = stayWithClass;
+}
+function stayWithClass() {
+    pElement.innerText = "Nah, the sparkles looked a little suspicious. You follow the class for the rest of the field trip, and nobody ever finds the wand. However, you go back to school with a mystery to talk to your friends about and some new experiences. Reload the page to play again!"
+    aButtonElement.innerText = "Go back to museum";
+    bButtonElement.innerText = "Go back to city";
+    aButtonElement.onclick = goToMuseum;
+    bButtonElement.onclick = goToCity;
+}
+function followSparkles() {
+    pElement.innerText = "The sparkles are just too shiny for your to resist. You trace the path they have left through the museum and out the museum door. And then you see it! It's Circe's ancient wand moving by itself, wiggling its way towards the road. 'Stop!' you yell at the wand, but it doesn't. You resort to casting a freeze spell on the wand. Once it's frozen, you grab it. Looking at it, you remember just how much this wand is worth. What to do, what to do...";
+    aButtonElement.innerText = "Give it back to the museum";
+    bButtonElement.innerText = "Sell it for money!";
+    aButtonElement.onclick = giveWandBack;
+    bButtonElement.onclick = sellWand;
+}
+function giveWandBack() {
+    pElement.innerText = "You decide to do the right thing and give the wand back to the museum. They thank you for finding it and shower you with good grades and blessings. Congratulations! Reload the page to play again.";
+    aButtonElement.innerText = "Go back to museum";
+    bButtonElement.innerText = "Go back to city";
+    aButtonElement.onclick = goToMuseum;
+    bButtonElement.onclick = goToCity;
+}
+function sellWand() {
+    pElement.innerText = "You let your greed get the better of you. Tsk tsk, we're all a little money-hungry inside I suppose. You sell the wand at the market and earn enough money to last you several lifetimes. With this money, you drop out of school (yay!) and hopefully donate to some good causes! Congratulations!! Reload the page to play again."
     aButtonElement.innerText = "Go back to museum";
     bButtonElement.innerText = "Go back to city";
     aButtonElement.onclick = goToMuseum;
