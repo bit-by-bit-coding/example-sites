9a2d4622d79809a5e323f0d599757aa11237753b
luciazhang00791 <67433805+luciazhang00791@users.noreply.github.com>
2020-08-10 23:51:21 -0700

    beginning and first node done

--- Basic Site/script.js
+++ Basic Site/script.js
@@ -2,6 +2,21 @@ let pElement = document.getElementById("messagBox");
 let aButtonElement = document.getElementById("aButton");
 let bButtonElement = document.getElementById("bButton");
 
+pElement.innerText = "Welcome to the game! You are a student wizard attending a prestigious school. Today, you are going on a field trip. Sounds kinda lame, so maybe you want to hang out with friends instead..."
+aButtonElement.innerText = "Go to field trip with class"
+bButtonElement.innerText = "Dip class, hang out with friends"
+aButtonElement.onclick = goToMuseum;
+bButtonElement.onclick = goWithFriends;
+function goToMuseum() {
+    pElement.innerText = "Looks like you're a good student! You go with your class to a museum, which seems kinda lame. But when you enter, you see thousands of ancient, magical relics. Turns out, one of the most prized, magical wands used by ancient sorceress Circe has been stolen! You remember passing by a suspicious hallway when you first walked in. Do you want to explore it or follow your classmates?";
+    aButtonElement.innerText = "Explore the hallway";
+    bButtonElement.innerText = "Follow your class";
+    aButtonElement.onclick = exploreHallway;
+    bButtonElement.onclick = followClass;
+}
+function exploreHallway() {
+    
+}
 /*
 start game in school, plot is like mystery/school trip
 decide between going with class or dipping w/some friends in their car or something (flying)
@@ -20,7 +35,7 @@ city -> market or arcade or
 arcade -> game of chance, quiz
 game of chance -> arcade, market
 quiz -> arcade, market
-market -> buy things, museum
+market -> buy things, go to class (find class)
 game of chance
 quiz
 character customization
