065ff9432e61076f5e186b8bf05bc7c99c077493
luciazhang00791 <67433805+luciazhang00791@users.noreply.github.com>
2020-08-10 23:44:35 -0700

    added plot in comments

--- Basic Site/script.js
+++ Basic Site/script.js
@@ -1,3 +1,29 @@
 let pElement = document.getElementById("messagBox");
 let aButtonElement = document.getElementById("aButton");
 let bButtonElement = document.getElementById("bButton");
+
+/*
+start game in school, plot is like mystery/school trip
+decide between going with class or dipping w/some friends in their car or something (flying)
+school -> museum
+friends can go to the city or to museum
+museum (magical ancient wand been stolen) -> explore dark room w/armor or follow class
+dark room -> hear a load noise, follow/find class
+follow -> die
+find class, stay for a while -> spy sparkles or something on floor, follow/continue
+continue -> go back to school, don't find it
+follow -> find wand!
+find wand -> sell, give back
+sell -> you get rich
+give back -> get good grades
+city -> market or arcade or 
+arcade -> game of chance, quiz
+game of chance -> arcade, market
+quiz -> arcade, market
+market -> buy things, museum
+game of chance
+quiz
+character customization
+inventory 
+market
+*/
